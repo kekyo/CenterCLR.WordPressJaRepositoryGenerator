3921c15311bb97e73a60900753f21c0775c34e1d
Kouji Matsui <k@kekyo.net>
2015-03-05 01:16:38 +0900

    Bit changed.

--- CenterCLR.WordPressJaRepositoryGenerator/Program.cs
+++ CenterCLR.WordPressJaRepositoryGenerator/Program.cs
@@ -1,4 +1,33 @@
-﻿using System;
+﻿////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// CenterCLR.WordPressJaRepositoryGenerator -
+//   Auto create and maint WordPress-ja Git repository.
+// Copyright (c) Kouji Matsui, All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+// THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+// IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+// EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
@@ -110,32 +139,52 @@ namespace CenterCLR.WordPressJaRepositoryGenerator
 			var urls = await GetWordPressJaDistributionFileUrlsAsync().ConfigureAwait(false);
 
 			var basePath = "WordPress-ja";
-			if (Directory.Exists(basePath) == true)
+			if (Directory.Exists(basePath) == false)
 			{
-				Directory.Delete(basePath, true);
+				Repository.Init(basePath, false);
 			}
-
-			Repository.Init(basePath, false);
 
 			using (var r = new Repository(basePath))
 			{
+				var tags = new HashSet<string>(r.Tags.Select(tag => tag.Name));
+
 				foreach (var url in urls)
 				{
+					var versionString = string.Join("-", Path.GetFileNameWithoutExtension(url.LocalPath).Split('-').Skip(1));
+					if (tags.Contains(versionString) == true)
+					{
+						Console.WriteLine(url.ToString() + " already fetched.");
+						continue;
+					}
+
+					Clean(basePath);
+
 					await FetchAndExtractWordPressDistributionAsync(url, basePath).ConfigureAwait(false);
 
 					r.Stage("*");
-					r.Commit(url.ToString());
+					var commit = r.Commit(url.ToString());
 
-					Console.WriteLine(url.ToString());
+					r.ApplyTag(versionString);
 
-					Clean(basePath);
+					Console.WriteLine(url.ToString() + " fetched.");
 				}
 			}
 		}
 
 		static void Main(string[] args)
 		{
+			Console.WriteLine("CenterCLR.WordPressJaRepositoryGenerator - Auto create and maint WordPress-ja Git repository.");
+			Console.WriteLine("Copyright (c) Kouji Matsui, All rights reserved.");
+			Console.WriteLine();
+
+			Console.WriteLine("Fetching from WordPress-ja sites...");
+			Console.WriteLine();
+
 			ExecuteAsync().Wait();
+
+			Console.WriteLine();
+
+			Console.WriteLine("Done.");
 		}
 	}
 }
